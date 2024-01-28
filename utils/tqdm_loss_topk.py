5024ba43f07bb29a8ec29c0f7b903bd906a3871c
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2024-01-28 17:54:58 +0900

    split tqdm_loss_topk into smaller methods

--- utils/tqdm_loss_topk.py
+++ utils/tqdm_loss_topk.py
@@ -5,6 +5,10 @@ from tqdm import tqdm
 
 class TqdmLossTopK(tqdm):
 
+    def __init__(self):
+        super().__init__()
+        self.postfix_str = ""
+
     def set_postfix_str_loss_topk(
         self,
         global_step: int,
@@ -14,11 +18,23 @@ class TqdmLossTopK(tqdm):
     ) -> str:
         assert len(topk) == len(topk_values)
 
-        postfix_str = f"step={global_step:d}, "
-        postfix_str += f"loss={loss:6.4e} "
+        self.add_step(global_step)
+        self.add_loss(loss)
+        self.add_topk(topk_values, topk)
+
+        self.set_postfix_str(self.postfix_str)
+        return self.postfix_str
+
+    def add_step(self, step: int) -> None:
+        self.postfix_str += f"step={step:d}, "
+
+    def add_loss(self, loss: float) -> None:
+        self.postfix_str += f"loss={loss:6.4e} "
+
+    def add_topk(
+        self,
+        topk_values: Tuple[float, ...],
+        topk: Tuple[int, ...]
+    ) -> None:
         for k, value in zip(topk, topk_values):
-            postfix_str += f"top{k}={value:6.2f} "
-
-        self.set_postfix_str(postfix_str)
-
-        return postfix_str
+            self.postfix_str += f"top{k}={value:6.2f} "
