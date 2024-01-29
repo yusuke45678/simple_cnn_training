e36471826ee5df09743cf0e4fa7274c328f05c0e
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2024-01-29 23:46:07 +0900

    add init_postfix_str() to TqdmLossTopK

--- utils/tqdm_loss_topk.py
+++ utils/tqdm_loss_topk.py
@@ -18,21 +18,24 @@ class TqdmLossTopK(tqdm):
     ) -> str:
         assert len(topk) == len(topk_values)
 
-        self.postfix_str = ""
-        self.add_step(global_step)
-        self.add_loss(loss)
-        self.add_topk(topk_values, topk)
+        self.init_postfix_str()
+        self.add_step_to_postfix_str(global_step)
+        self.add_loss_to_postfix_str(loss)
+        self.add_topk_to_postfix_str(topk_values, topk)
 
         self.set_postfix_str(self.postfix_str)
         return self.postfix_str  # type: ignore[no-any-return]
 
-    def add_step(self, step: int) -> None:
+    def init_postfix_str(self) -> None:
+        self.postfix_str = ""
+
+    def add_step_to_postfix_str(self, step: int) -> None:
         self.postfix_str += f"step={step:d}, "
 
-    def add_loss(self, loss: float) -> None:
+    def add_loss_to_postfix_str(self, loss: float) -> None:
         self.postfix_str += f"loss={loss:6.4e} "
 
-    def add_topk(
+    def add_topk_to_postfix_str(
         self,
         topk_values: Tuple[float, ...],
         topk: Tuple[int, ...]
