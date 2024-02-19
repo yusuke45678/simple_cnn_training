9eb45d97baaa2833b9dc585b3153a76379a035a6
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2024-02-19 12:29:35 +0900

    add test for optimizer and scheduler

---
+++ test/setup/test_optimizer.py
@@ -0,0 +1,26 @@
+import pytest
+from torch.optim import Optimizer
+
+from setup import configure_optimizer
+
+
+@pytest.mark.parametrize('optimizer_name', ["SGD", "Adam"])
+@pytest.mark.parametrize('lr', [0.01, 1e-5])
+@pytest.mark.parametrize('weight_decay', [0.01, 1e-5])
+@pytest.mark.parametrize('momentum', [1.0, 0.99])
+def test_optimizer(
+    optimizer_name,
+    lr,
+    weight_decay,
+    momentum,
+    model,
+):
+
+    optimizer = configure_optimizer(
+        optimizer_name=optimizer_name,
+        lr=lr,
+        weight_decay=weight_decay,
+        momentum=momentum,
+        model_params=model.get_parameters()
+    )
+    assert isinstance(optimizer, Optimizer)
