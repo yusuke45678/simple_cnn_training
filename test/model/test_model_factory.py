1fe5a1baf6c8788a5a6168941f570b2c898c0d44
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2024-02-19 13:07:44 +0900

    add test_model_factory

---
+++ test/model/test_model_factory.py
@@ -0,0 +1,26 @@
+import pytest
+import torch
+
+from model import configure_model, ModelConfig, BaseModel
+
+
+@pytest.mark.parametrize(
+    'model_name',
+    ["resnet18", "resnet50", "abn_r50", "vit_b", "x3d", "zero_output_dummy"]
+)
+@pytest.mark.parametrize('n_classes', [2, 10])
+@pytest.mark.parametrize('use_pretrained', [True, False])
+def test_model_factory(
+    model_name,
+    n_classes,
+    use_pretrained,
+):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = configure_model(ModelConfig(
+        model_name=model_name,
+        use_pretrained=use_pretrained,
+        n_classes=n_classes,
+        device=device
+    ))
+    assert isinstance(model, BaseModel)
